74da7e6a806c5b850f62ad9fb8bcd06ceb200d35
Michael Harris <maharris84@hotmail.com>
2021-02-14 21:25:12 -0800

    Update database.php

--- scripts/database.php
+++ scripts/database.php
@@ -1,7 +1,7 @@
 <?php
   
-  $user = 'mike';            //$user = 'rpuser';
-  $pass = 'mah1384MAH!#*$'; //$pass = 'rplan1#8$';
+  $user = 'raceplan';
+  $pass = 'rplanner'; // insecure but fine for localhost only access during dev
   
   try {
       $db = new PDO('mysql:host=localhost;dbname=raceplan', $user, $pass);
